0cec2b3d7f8858c851f316347df3bdb223203352
shnasta <shvets.nasta@gmail.com>
2022-10-13 23:04:17 +0300

    added CharacterFactory

--- src/main/java/CharacterFactory.java
+++ src/main/java/CharacterFactory.java
@@ -1,22 +1,17 @@
 import char_dir.Character;
+import lombok.SneakyThrows;
 import org.reflections.*;
 
 import java.util.Random;
 import java.util.Set;
 
 public class CharacterFactory {
-    void createCharacter() {
+    @SneakyThrows
+    Character createCharacter() {
         Reflections reflections = new Reflections("");
         Set<Class<? extends Character>> subTypes = reflections.getSubTypesOf(Character.class);
-        Random rn = new Random();
-        int index = rn.nextInt(subTypes.size());
-        Object[] myCharact = subTypes.toArray();
-        System.out.println(myCharact);
-    }
-
-    public static void main(String[] args) {
-        CharacterFactory a = new CharacterFactory();
-        a.createCharacter();
+        Object randomCharacter = subTypes.toArray()[new Random().nextInt(subTypes.size())];
+        return (Character) randomCharacter.getClass().getConstructor().newInstance();
     }
 }
 
